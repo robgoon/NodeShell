3a5310f8777bd5a1ccc66b5fe3709f042476cb04
Robert Goon <rob.goon@gmail.com>
2018-03-06 12:07:22 -0500

    pass

--- bash.js
+++ bash.js
@@ -1,9 +1,13 @@
-process.stdout.write('promt > ')
+process.stdout.write('prompt > ')
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim()
 
+  if (cmd === 'pwd'){
+    process.stdout.write('Current Directory: ' + process.cwd() + '\n')
+  }
+
   process.stdout.write('You Typed: ' + cmd)
-  process.stdout.write('\npromt >')
+  process.stdout.write('\nprompt >')
 
 })
